921d0db8fb18af25e55cc9a71fc9518bae053a53
Ashurov Safarmurod <sm.ashurov7@gmail.com>
2026-02-28 05:27:59 +0100

    DiaryEntry changed

--- Models/DiaryEntry.cs
+++ Models/DiaryEntry.cs
@@ -14,9 +14,9 @@ namespace backend.Models
         public string? AudioFileUri { get; set; }
         public string RawText { get; set; } = string.Empty;
 
-        public int? MoodScore { get; set; }
-        public int? StressLevel { get; set; }
-        public int? EnergyLevel { get; set; }
+        public int MoodScore { get; set; }
+        public int StressLevel { get; set; }
+        public int EnergyLevel { get; set; }
 
         public string Category { get; set; } = "General";
         public string AiPatientFeedback { get; set; } = string.Empty;
